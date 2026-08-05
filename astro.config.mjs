0e52cd41b9a744d66ffda1e774d07a4ace6d8c96
azizelkateb <azizkateb69@gmail.com>
2026-08-05 10:46:31 +0100

    responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://muhammadeldemerdash.github.io",
-  base: "/team-salon-alolaya",
   output: "static",
   trailingSlash: "always",
   compressHTML: false,
